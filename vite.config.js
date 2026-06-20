b994c0b522d0a069177f74aecc452a6218f74b69
MatiasC19 <66331535+MatiasC19@users.noreply.github.com>
2026-06-20 16:20:49 -0300

    Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom'],
+          'vendor-motion': ['framer-motion'],
+          'vendor-firebase': ['firebase/app', 'firebase/database'],
+        },
+      },
+    },
+  },
+});
